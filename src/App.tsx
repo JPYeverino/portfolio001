e3d94a72f85a8bfffadb0db5e63144054b10fdf9
Juan Pablo Yeverino Morales <jyeverinomor@svb.com>
2020-07-23 20:08:09 -0700

    Very first commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="first-container">
+      <h1 className="first-line">
+      J. Pablo Yeverino Portfolio
+    </h1>
+    <h3 className="subtitle">
+      A journey of self commitment.
+    </h3>
+    <p className="project-description">
+      I want to create my portfolio from scratch using Reactjs, Angular 2+ and Nodejs for REST and GraphQl apis. I will set little tasks daily so I can push myself to finish this project. Below you will see the tasks. 
+    </p>
+    <ul className="tasks-list">
+      <li className="task-item">Deploy this simple web page using AWS and github. If you can see this, I already accomplished <span>July, 23rd - 20:05</span></li>
+    </ul>
     </div>
   );
 }
